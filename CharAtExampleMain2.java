0bdccecc5e65b382a88ee92b2a92bb25d877f53e
Irfana995 <58158728+Irfana995@users.noreply.github.com>
2019-12-18 15:19:01 +0530

    Add files via upload

---
+++ CharAtExampleMain2.java
@@ -0,0 +1,11 @@
+package stringMethod;
+
+public class CharAtExampleMain2 
+{
+	public static void main(String[] args)
+	{
+		String name="FAce@2.0";
+		char ch=name.charAt(10);//returns the char value at the 10th index
+		System.out.println(ch);
+	}
+}
